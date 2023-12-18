774e1c567f0ec9c7caf347f6e7c7e674b0d68a99
nittalasaikalyan <sai.kalyan42@gmail.com>
2023-12-18 15:49:58 +0530

    updated instance size

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "app_server" {
   ami           = "ami-0759f51a90924c166"
-  instance_type = "t2.xlarge"
-  count = 5
+  instance_type = "t2.micro"
+  count = 2
   tags = {
     Name = "test1,test2"
   }
