68705ececf2705af49ac0107253dcebb360c91d3
nittalasaikalyan <sai.kalyan42@gmail.com>
2023-12-18 15:42:11 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 resource "aws_instance" "app_server" {
   ami           = "ami-0759f51a90924c166"
   instance_type = "t2.xlarge"
-
+  count = 5
   tags = {
-    Name = "user23"
+    Name = "test1,test2"
   }
 }
+
+
