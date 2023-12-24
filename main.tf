d3949d7d902eb56d50506ba681e5276e10e11fcf
nittalasaikalyan <sai.kalyan42@gmail.com>
2023-12-24 16:37:27 +0530

    user data additon

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucketi231220203hfgfg"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Prod"
-  }
-}
 resource "aws_instance" "app_server" {
   ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"
@@ -14,3 +6,9 @@ resource "aws_instance" "app_server" {
     Name = "ExampleAppServerInstance"
   }
 }
+
+userdata:
+    #!/bin/bash
+    sudo apt update
+    sudo apt install nginx -y
+    sudo systemctl enable nginx
