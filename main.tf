5c2bcadba055a8a03a2c3a8bbb181d9bc661047a
nittalasaikalyan <sai.kalyan42@gmail.com>
2023-12-24 16:21:03 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-0759f51a90924c166"
-  instance_type = "t2.micro"
-  count = 2
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucketi231220203hfgfg"
+
   tags = {
-    Name = "test1,test2"
+    Name        = "My bucket"
+    Environment = "Prod"
   }
 }
+resource "aws_instance" "app_server" {
+  ami           = "ami-0fc5d935ebf8bc3bc"
+  instance_type = "t2.micro"
 
-
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
