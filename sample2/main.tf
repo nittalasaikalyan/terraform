a371d2c63a2b0f8dfc9958e1ffa587efe12eee00
nittalasaikalyan <sai.kalyan42@gmail.com>
2023-12-27 12:49:32 +0530

    updated

--- sample2/main.tf
+++ sample2/main.tf
@@ -1,21 +1,21 @@
 #resource "aws_s3_bucket" "example" {
- # bucket = "kalyan-bucket-24.12.2023"
+  #bucket = "kalyan-bucket-24.12.2023"
 
   #tags = {
-   # Name        = "My bucket"
+    #Name        = "My bucket"
     #Environment = "Prod"
- # }
+  #}
 #}
 #resource "aws_instance" "app_server" {
- # count = 2
- # ami           = var.ami_id
- # instance_type = "t2.micro"
- # key_name = "user23"
- # security_groups = [aws_security_group.example.name]
+  #count = 2
+  #ami           = var.ami_id
+  #instance_type = "t2.micro"
+  #key_name = "user23"
+  #security_groups = [aws_security_group.example.name]
 
- # tags = {
-   # Name = "instancecount.${count.index}"
- # }
+  #tags = {
+    #Name = "instancecount.${count.index}"
+  #}
 #user_data= file("${path.module}/nginx.sh")
 #}
 #resource "aws_security_group" "example" {
@@ -23,25 +23,25 @@
  #description = "Allow HTTPS to web server"
 
 #ingress {
-  # description = "HTTPS ingress"
-  # from_port   = 22
-  # to_port     = 22
-  # protocol    = "tcp"
-  # cidr_blocks = ["0.0.0.0/0"]
-# }
+   #description = "HTTPS ingress"
+   #from_port   = 22
+   #to_port     = 22
+   #protocol    = "tcp"
+   #cidr_blocks = ["0.0.0.0/0"]
+ #}
 #ingress {
-  # description = "HTTPS ingress"
-  # from_port   = 80
-  # to_port     = 80
-  # protocol    = "tcp"
-  # cidr_blocks = ["0.0.0.0/0"]
-# }
+   #description = "HTTPS ingress"
+   #from_port   = 80
+   #to_port     = 80
+   #protocol    = "tcp"
+   #cidr_blocks = ["0.0.0.0/0"]
+ #}
 #egress {
-  # from_port   = 0
-  # to_port     = 0
-  # protocol    = "-1"
-  # cidr_blocks = ["0.0.0.0/0"]
-# }
+   #from_port   = 0
+   #to_port     = 0
+   #protocol    = "-1"
+   #cidr_blocks = ["0.0.0.0/0"]
+ #}
 #}
 resource "aws_vpc" "main" {
   cidr_block       = "16.0.0.0/16"
